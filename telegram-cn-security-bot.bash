4b18ae7b91b24d06f6f1ad7f30c1a78cf06044c6
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-12-13 12:12:46 +0800

    Prepare for build

--- telegram-cn-security-bot.bash
+++ telegram-cn-security-bot.bash
@@ -1,39 +1,69 @@
 #!/bin/bash
-cd `dirname $BASH_SOURCE`
-name=`basename $BASH_SOURCE | cut -d . -f1`
-command="java -jar $name.jar"
-function doStart {
-	pid=`pidof $name`
-	if [ ! $pid ] ; then
-		rm $name.out $name.err
-		bash -c "exec -a $name $command &" 1>>$name.out 2>>$name.err
-	fi
-	pid=`pidof $name`
-	echo $pid started
-}
-function doStop {
-	pid=`pidof $name`
-	if [ $pid ] ; then
-		echo $pid stopped
-		kill $pid
-	fi
-}
-trap 'onCtrlC' INT
-function onCtrlC {
-	doStop
-}
-if [ "$1" == "stop" ] ; then
-	doStop
-	exit
-elif [ "$1" == "start" ] ; then
-	doStart
-	exit
-elif [ "$1" == "restart" ] ; then
-	doStop
-	sleep 1
-	doStart
-	exit
+# BASh launcher with stdout and stderr to files
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+name=$(basename "${BASH_SOURCE[0]}")
+name=${name%.*}
+
+if [ ! -x "$name" ]; then
+  # Exec
+  chmod +x "$name"
+  command="./$name"
+elif [ ! -f "$name.jar" ]; then
+  # Java
+  command="java -jar $name.jar"
+elif [ ! -f "$name.py" ]; then
+  # Java
+  command="python -u $name.py"
 else
-	doStart
-	tail --pid=$pid -qf $name.out $name.err
+  echo "Program file not found!"
+  exit 1
+fi
+
+function doStart() {
+  pid=$(pidof "$name")
+  if [ ! "$pid" ]; then
+    rm "$name.out" "$name.err"
+    bash -c "exec -a $name $command &" 1>>"$name.out" 2>>"$name.err"
+  fi
+  sleep 1
+  pid=$(pidof "$name")
+  if [ "$pid" ]; then
+    echo "$pid started"
+  else
+    cat "$name.out" "$name.err"
+    exit 10
+  fi
+}
+
+function doStop() {
+  pid=$(pidof "$name")
+  # shellcheck disable=SC2086
+  if [ "$pid" ]; then
+    kill $pid
+    while kill -0 $pid 2>/dev/null; do
+      sleep 1
+    done
+    echo "$pid stopped"
+  fi
+}
+
+trap 'onCtrlC' INT
+
+function onCtrlC() {
+  doStop
+}
+
+if [ "$1" == "stop" ]; then
+  doStop
+  exit
+elif [ "$1" == "start" ]; then
+  doStart
+  exit
+elif [ "$1" == "restart" ]; then
+  doStop
+  doStart
+  exit
+else
+  doStart
+  tail --pid="$pid" -qf "$name.out" "$name.err"
 fi
